04c778a698f5e59989a05b452ac34d9a67687b7a
WuTao <CNWuTao@outlook.com>
2020-05-31 00:52:25 +0800

    基本功能实现

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Lbl_InstallPath = new System.Windows.Forms.Label();
+            this.TextBox_AEInstallPath = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Btn_BrowseFolder = new System.Windows.Forms.Button();
+            this.ComboBox_LanguageList = new System.Windows.Forms.ComboBox();
+            this.Btn_SetLanguage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CheckBox_LaunchApp = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Lbl_InstallPath
+            // 
+            this.Lbl_InstallPath.AutoSize = true;
+            this.Lbl_InstallPath.Location = new System.Drawing.Point(3, 8);
+            this.Lbl_InstallPath.Margin = new System.Windows.Forms.Padding(3, 8, 3, 0);
+            this.Lbl_InstallPath.Name = "Lbl_InstallPath";
+            this.Lbl_InstallPath.Size = new System.Drawing.Size(91, 15);
+            this.Lbl_InstallPath.TabIndex = 0;
+            this.Lbl_InstallPath.Text = "AE 安装路径";
+            // 
+            // TextBox_AEInstallPath
+            // 
+            this.TextBox_AEInstallPath.Location = new System.Drawing.Point(100, 3);
+            this.TextBox_AEInstallPath.Name = "TextBox_AEInstallPath";
+            this.TextBox_AEInstallPath.Size = new System.Drawing.Size(276, 25);
+            this.TextBox_AEInstallPath.TabIndex = 1;
+            this.TextBox_AEInstallPath.TextChanged += new System.EventHandler(this.TextBox_AEInstallPath_TextChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.Lbl_InstallPath);
+            this.flowLayoutPanel1.Controls.Add(this.TextBox_AEInstallPath);
+            this.flowLayoutPanel1.Controls.Add(this.Btn_BrowseFolder);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(500, 42);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // Btn_BrowseFolder
+            // 
+            this.Btn_BrowseFolder.Location = new System.Drawing.Point(382, 3);
+            this.Btn_BrowseFolder.Name = "Btn_BrowseFolder";
+            this.Btn_BrowseFolder.Size = new System.Drawing.Size(113, 29);
+            this.Btn_BrowseFolder.TabIndex = 2;
+            this.Btn_BrowseFolder.Text = "手动选择";
+            this.Btn_BrowseFolder.UseVisualStyleBackColor = true;
+            this.Btn_BrowseFolder.Click += new System.EventHandler(this.Btn_BrowseFolder_Click);
+            // 
+            // ComboBox_LanguageList
+            // 
+            this.ComboBox_LanguageList.FormattingEnabled = true;
+            this.ComboBox_LanguageList.Items.AddRange(new object[] {
+            "",
+            "简体中文",
+            "英语"});
+            this.ComboBox_LanguageList.Location = new System.Drawing.Point(112, 60);
+            this.ComboBox_LanguageList.Name = "ComboBox_LanguageList";
+            this.ComboBox_LanguageList.Size = new System.Drawing.Size(121, 23);
+            this.ComboBox_LanguageList.TabIndex = 3;
+            // 
+            // Btn_SetLanguage
+            // 
+            this.Btn_SetLanguage.BackColor = System.Drawing.Color.DodgerBlue;
+            this.Btn_SetLanguage.FlatAppearance.BorderSize = 0;
+            this.Btn_SetLanguage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_SetLanguage.ForeColor = System.Drawing.Color.White;
+            this.Btn_SetLanguage.Location = new System.Drawing.Point(394, 116);
+            this.Btn_SetLanguage.Name = "Btn_SetLanguage";
+            this.Btn_SetLanguage.Size = new System.Drawing.Size(118, 39);
+            this.Btn_SetLanguage.TabIndex = 4;
+            this.Btn_SetLanguage.Text = "确定";
+            this.Btn_SetLanguage.UseVisualStyleBackColor = false;
+            this.Btn_SetLanguage.Click += new System.EventHandler(this.Btn_SetLanguage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 8, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择语言：";
+            // 
+            // CheckBox_LaunchApp
+            // 
+            this.CheckBox_LaunchApp.AutoSize = true;
+            this.CheckBox_LaunchApp.Checked = true;
+            this.CheckBox_LaunchApp.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckBox_LaunchApp.Location = new System.Drawing.Point(265, 64);
+            this.CheckBox_LaunchApp.Name = "CheckBox_LaunchApp";
+            this.CheckBox_LaunchApp.Size = new System.Drawing.Size(75, 19);
+            this.CheckBox_LaunchApp.TabIndex = 5;
+            this.CheckBox_LaunchApp.Text = "启动AE";
+            this.CheckBox_LaunchApp.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(524, 167);
+            this.Controls.Add(this.CheckBox_LaunchApp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_SetLanguage);
+            this.Controls.Add(this.ComboBox_LanguageList);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "AE 中英文切换工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Lbl_InstallPath;
+        private System.Windows.Forms.TextBox TextBox_AEInstallPath;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button Btn_BrowseFolder;
+        private System.Windows.Forms.ComboBox ComboBox_LanguageList;
+        private System.Windows.Forms.Button Btn_SetLanguage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox CheckBox_LaunchApp;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,14 +6,169 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32; //注册表操作
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace AELanguageSwitcher
 {
     public partial class Form1 : Form
     {
+        private const string AE_INSTALLPATH = @"HKEY_LOCAL_MACHINE\SOFTWARE\Adobe\After Effects";
+        private const string AE_ConfigFileName = "painter.ini";
+        private const string AE_ExecutionName = "AfterFX.exe";
+        private const string AE_Lang_EN = "en_US";
+        private const string AE_Lang_CN = "zh_CN";
+        private const string AppConfigPath = "AppConfig.ini";
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //TextBox_AEInstallPath.Text = GetAEInstallPath();
+            if(File.Exists(AppConfigPath))
+            {
+                StringBuilder sb = new StringBuilder();
+                ProfileHelper.GetPrivateProfileString("Main", "Path", "", sb, 512, AppConfigPath);
+                TextBox_AEInstallPath.Text = sb.ToString();
+            }
+        }
+
+        private string GetAEInstallPath()
+        {
+            RegistryKey AEPathFinder;
+            try
+            {
+                //使用只读模式打开
+                AEPathFinder = Registry.LocalMachine.OpenSubKey(AE_INSTALLPATH,false);
+                var finder1 = AEPathFinder.OpenSubKey(AEPathFinder.GetSubKeyNames()[0]);
+
+                return finder1.GetValue("InstallPath", string.Empty).ToString();
+            }
+            catch(Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+        private void Btn_BrowseFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                TextBox_AEInstallPath.Text = dialog.SelectedPath;
+                if(!File.Exists(AppConfigPath))
+                {
+                    File.Create(AppConfigPath);
+                }
+                //保存路径
+                ProfileHelper.WritePrivateProfileString("Main", "Path", dialog.SelectedPath, AppConfigPath);
+
+                string profile = TextBox_AEInstallPath.Text + "\\" + AE_ConfigFileName;
+                ReadConfig(profile);
+            }
+        }
+
+        private void ReadConfig(string profile)
+        {
+            if (File.Exists(profile))
+            {
+                File.SetAttributes(profile, FileAttributes.Normal);
+                int size = 512;
+                StringBuilder sb = new StringBuilder(size);
+                ProfileHelper.WritePrivateProfileString("Config", "ForceLanguage", "1", profile);
+                ProfileHelper.GetPrivateProfileString("Config", "Language", "", sb, size, profile);
+                if (sb.ToString() == "")
+                {
+                    ComboBox_LanguageList.SelectedIndex = 0;
+                }
+                else if (sb.ToString().Trim().ToUpper() == "ZH_CN")
+                {
+                    ComboBox_LanguageList.SelectedIndex = 1;
+                }
+                else if (sb.ToString().Trim().ToUpper() == "EN_US")
+                {
+                    ComboBox_LanguageList.SelectedIndex = 2;
+                }
+            }
+        }
+
+
+        private void Btn_SetLanguage_Click(object sender, EventArgs e)
+        {
+            string profile = TextBox_AEInstallPath.Text + "\\" + AE_ConfigFileName;
+            if(File.Exists(profile))
+            {
+                if(ComboBox_LanguageList.SelectedIndex == 1)
+                {
+                    ProfileHelper.WritePrivateProfileString("Config", "Language", "zh_CN", profile);
+                }
+                else if(ComboBox_LanguageList.SelectedIndex == 2)
+                {
+                    ProfileHelper.WritePrivateProfileString("Config", "Language", "en_US", profile);
+                }
+            }
+
+            if (CheckBox_LaunchApp.Checked == true)
+            {
+                string p = TextBox_AEInstallPath.Text + "\\" + AE_ExecutionName;
+                if(File.Exists(p))
+                {
+                    try
+                    {
+                        Process.Start(p);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                    
+                }
+            }
+        }
+
+        private void TextBox_AEInstallPath_TextChanged(object sender, EventArgs e)
+        {
+            string p = TextBox_AEInstallPath.Text + "\\" + AE_ConfigFileName;
+            if(File.Exists(p))
+            {
+                ReadConfig(p);
+            }
+        }
     }
+
+    /// <summary>
+    /// INI文件操作类
+    /// </summary>
+    public class ProfileHelper
+    {
+        /// <summary>
+        /// 读 INI 文件
+        /// </summary>
+        /// <param name="lpAppName">节点名称</param>
+        /// <param name="lpKeyName">项名称</param>
+        /// <param name="lpDefault">没有找到指定项时的返回值</param>
+        /// <param name="lpReturnString">指定一个字符缓冲区，长度最少为nSize</param>
+        /// <param name="nSize">指定复制到字符缓冲区的最大字符数量</param>
+        /// <param name="lpFileName">ini文件路径</param>
+        /// <returns>复制到字符缓冲区的字节数，不包括中止字符</returns>
+        [DllImport("kernel32")]
+        public static extern int GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnString, int nSize, string lpFileName);
+
+        /// <summary>
+        /// 写 INI 文件
+        /// </summary>
+        /// <param name="lpSectionName">要写入的节点名称</param>
+        /// <param name="lpKeyName">要写入的项目名称</param>
+        /// <param name="lpString">要写入的字符串</param>
+        /// <param name="lpFileName">ini文件的路径</param>
+        /// <returns>零表示失败，非零表示成功</returns>
+        [DllImport("kernel32")]
+        public static extern int WritePrivateProfileString(string lpSectionName, string lpKeyName, string lpString, string lpFileName);
+    }
+
 }
